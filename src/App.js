57a204d26025b42577c9d90d6c120aa08b261cbe
carmelius <carmelius.umbah@icloud.com>
2023-12-07 13:00:54 -0800

    hii

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <html>
+      <body>
+        <h1>welcome to aws amplification</h1>
+        <p>welcome to aws and deploy</p>
+             </body>
+             </html>
+    
+  );
+}
+
+export default App;
